05f3218d9865529549a3f6408c5a2c686ab2c325
OFIReyes <113851092+reyeees@users.noreply.github.com>
2024-11-08 18:47:52 +0100

    Delete br directory
    Trash. Will be deleted in release v1.2

--- br/background.js
+++
@@ -1,8 +0,0 @@
-chrome.action.onClicked.addListener((tab) => {
-    chrome.scripting.executeScript({
-      target: {tabId: tab.id},
-      function: () => {
-        alert(getSelection().toString());
-      }
-    });
-  });
